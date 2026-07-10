36bcbc6d7d81be1b4e19e65b0ad0f734045ff6be
Vinayaka P M <vinayakapm3011@gmail.com>
2026-07-10 23:37:45 +0530

    Add python variables example

---
+++ 01_Basics/variables.py
@@ -0,0 +1,24 @@
+"""
+Program: Variables in Python
+Author: Vinayaka PM
+Description: Demonstrates different types of variables in Python.
+"""
+
+# Integer
+age = 22
+
+# Float
+cgpa = 8.21
+
+# String
+name = "Vinayaka PM"
+
+# Boolean
+is_student = True
+
+print("Student Details")
+print("----------------")
+print("Name :", name)
+print("Age :", age)
+print("CGPA :", cgpa)
+print("Student :", is_student)
