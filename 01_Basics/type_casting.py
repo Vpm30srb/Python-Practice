495c3e50ef8b3a133423ccedbd69699b065cf70c
Vinayaka P M <vinayakapm3011@gmail.com>
2026-07-10 23:44:30 +0530

    Add Python type casting examples

---
+++ 01_Basics/type_casting.py
@@ -0,0 +1,24 @@
+"""
+Program: Type Casting in Python
+Author: Vinayaka PM
+Description: Demonstrates explicit type casting.
+"""
+
+# String to Integer
+num1 = "100"
+print("String:", num1)
+
+num2 = int(num1)
+print("Integer:", num2)
+
+# Integer to Float
+age = 22
+print(float(age))
+
+# Float to Integer
+cgpa = 8.21
+print(int(cgpa))
+
+# Integer to String
+number = 50
+print(str(number))
