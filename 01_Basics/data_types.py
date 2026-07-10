d94668648d65f6230baa9ae0b6e0101e2686db53
Vinayaka P M <vinayakapm3011@gmail.com>
2026-07-10 23:40:41 +0530

    Add Python data types examples

---
+++ 01_Basics/data_types.py
@@ -0,0 +1,41 @@
+"""
+Program: Data Types in Python
+Author: Vinayaka PM
+Description: Demonstrates different built-in data types.
+"""
+
+# Integer
+num = 100
+
+# Float
+price = 99.99
+
+# String
+course = "Python"
+
+# Boolean
+is_completed = False
+
+# List
+languages = ["Python", "Java", "JavaScript"]
+
+# Tuple
+coordinates = (10, 20)
+
+# Dictionary
+student = {
+    "Name": "Vinayaka",
+    "CGPA": 8.21
+}
+
+# Set
+numbers = {1, 2, 3, 4}
+
+print(type(num))
+print(type(price))
+print(type(course))
+print(type(is_completed))
+print(type(languages))
+print(type(coordinates))
+print(type(student))
+print(type(numbers))
